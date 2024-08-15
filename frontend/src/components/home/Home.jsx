763d30ec91752d5496c748d1bfd96c60c96529c2
João Vithor <joaovithormg@gmail.com>
2024-08-15 19:14:20 -0300

    adding router

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from '../template/Main';
+
+export default props => 
+    <Main icon="home" title="Início" subtitle="Projeto para treinar crud">
+            <div className='display-4'>
+                Bem vindo! </div>
+                <hr />
+                <p className="mb-0">Sistema para exemplificar a construção de um cadastro de usuário com React</p>
+        </Main>
--- frontend/src/components/main/App.jsx
+++ frontend/src/components/main/App.jsx
@@ -2,17 +2,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css'
 import React from "react";
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 // importando os componentes
 import Logo from '../template/Logo';
 import Nav from '../template/Nav';
 import Home from '../home/Home';
 import Footer from '../template/Footer';
+import Routes from './Routes';
 
 export default props => 
+    <BrowserRouter>
     <div className="app">
         <Logo />
         <Nav />
-        <Home />
+        <Routes />
         <Footer />
     </div>
+    </BrowserRouter>
